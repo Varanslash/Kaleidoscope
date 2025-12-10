a193f4c0b50e63d868150cc65fe5f761a0dba744
Varanslash <odiaenvy@gmail.com>
2025-12-10 05:58:38 -0800

    V 1.0, Brainfuck-style Engine

---
+++ Kaleidoscope.py
@@ -0,0 +1,216 @@
+import time
+
+placeholder1 = 0
+placeholder2 = 0
+placeholder3 = 0
+counter = 0
+locationstack = []
+syntax = {
+    "x": "pass"
+}
+# ZEROEXT Parser/Lexer
+
+while True:
+    n = 0
+    i = 0
+    userinput = input("\n>>>0;")
+    try:
+        while n < len(userinput):
+            ch = userinput[n]
+            match ch:
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case "x":
+                    n += 1
+                case _:
+                    n += 1
+
+        while i < len(userinput):
+            ch = userinput[i]
+            match ch:
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case "x":
+                    code = syntax[ch]
+                    exec(code)
+                    i += 1
+                case _:
+                    i += 1
+    except Exception:
+        pass
